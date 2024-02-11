16cfdbaa6a59ad4fc9565c03443d95d344f22709
Zihan Yang <ws704603301@gmail.com>
2024-02-11 12:53:30 -0500

    Now the car's color changes when it has package in the car

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -6,24 +6,36 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
     [SerializeField] float destroyDelay = 0.5f;
     bool havePackage = false;
 
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Collision Detected!");
     }
 
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Package" && !havePackage) 
-        {  
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Package" && !havePackage)
+        {
             Debug.Log("Pacakage Picked Up!");
             havePackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyDelay);
         }
         if (other.tag == "Customer" && havePackage)
         {
             Debug.Log("Package Delivered!");
+            spriteRenderer.color = noPackageColor;
             havePackage = false;
         }
     }
